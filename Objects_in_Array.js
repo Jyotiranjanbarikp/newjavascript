c91980ffbf616ad0a875961f7c878679ac769cb6
Jyotiranjanbarikp <152086300+Jyotiranjanbarikp@users.noreply.github.com>
2023-12-07 17:41:29 +0000

    nestedobject

---
+++ Objects_in_Array.js
@@ -0,0 +1,22 @@
+const arrayObj = [
+    {
+        id : 121,
+        location : "BBSR"
+    },
+    {
+        id: 122,
+        location : "Cuttack"
+    },
+    {
+        id : 123,
+        location : "puri"
+    },
+    {
+        id : 124,
+        location : "jagatsinghpur"
+    }
+]
+
+console.log(arrayObj);
+console.log(arrayObj[3]);
+console.log(arrayObj[2]);
