7c5e64cd3abad6bcc2c74c702704e5359a580171
Jyotiranjanbarikp <152086300+Jyotiranjanbarikp@users.noreply.github.com>
2023-12-07 16:37:57 +0000

    function

---
+++ function.js
@@ -0,0 +1,9 @@
+function addTwoNos(num1,num2) {
+    console.log(num1+num2)
+}
+addTwoNos(5,5)
+function addTwoNos(num1,num2) {
+    return num1+num2;
+}
+const result = addTwoNos(10,5);
+console.log(result);
