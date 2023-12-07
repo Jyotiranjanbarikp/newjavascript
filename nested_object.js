99b2ea11ca38eb9bc3a4f26a12ac53bfabd8d47e
Jyotiranjanbarikp <152086300+Jyotiranjanbarikp@users.noreply.github.com>
2023-12-07 17:24:00 +0000

    object

---
+++ nested_object.js
@@ -0,0 +1,13 @@
+const userName = {
+    userFullName : {
+        userDetails : {
+            fname : "Jyotiranjan",
+            lname : "Barik"
+        }
+    }
+}
+
+console.log(userName);
+console.log(userName.userFullName.userDetails.fname);
+console.log(userName.userFullName.userDetails.lname);
+
